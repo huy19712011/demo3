2ad33d00dd4109754baea92bf4911fe5265372c2
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-10-23 23:15:45 +0700

    JPA demo: using JPQL - updated table name

--- src/main/java/com/example/demo3/entity/Product.java
+++ src/main/java/com/example/demo3/entity/Product.java
@@ -2,8 +2,10 @@ package com.example.demo3.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "products")
 public class Product {
 
     @Id
