fb1238d65366bebc3c5870cde7fdcaa6e5c4dd4f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-10-23 23:03:51 +0700

    JPA demo: using native query

---
+++ src/main/java/com/example/demo3/Main.java
@@ -0,0 +1,25 @@
+package com.example.demo3;
+
+import com.example.demo3.entity.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+        EntityManager em = emf.createEntityManager();
+
+        //TypedQuery<Product> query = em.createQuery("select p from Product p", Product.class);
+        //List<Product> list = query.getResultList();
+        //list.forEach(System.out::println);
+
+        em.createNativeQuery("select * from products", Product.class).getResultList().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/example/demo3/entity/Product.java
@@ -0,0 +1,39 @@
+package com.example.demo3.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    private long id;
+    private String name;
+
+    public Product() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
